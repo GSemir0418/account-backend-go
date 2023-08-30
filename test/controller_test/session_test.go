bf4d849788d413f394931475adf65b4e196a7ff6
gsemir0418 <845217811@qq.com>
2023-08-30 19:47:29 +0800

    test: session api return jwt when vc matches

--- test/controller_test/session_test.go
+++ test/controller_test/session_test.go
@@ -1,8 +1,12 @@
 package controller_test
 
 import (
+	queries "account/config/sqlc"
+	"account/internal/database"
 	"account/internal/router"
+	"context"
 	"encoding/json"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -15,12 +19,23 @@ import (
 func TestSession(t *testing.T) {
 	// 初始化路由: 初始化路由，连接数据库，读取 viper 配置
 	r := router.New()
+	// 创建真实的验证码
+	email := "1@qq.com"
+	code := "1234"
+	q := database.NewQuery()
+	c := context.Background()
+	if _, err := q.CreateValidationCode(c, queries.CreateValidationCodeParams{
+		Email: email,
+		Code:  code,
+	}); err != nil {
+		log.Fatalln(err)
+	}
 	// 响应记录器
 	w := httptest.NewRecorder()
 	// json 请求体
 	reqBody := gin.H{
-		"email": "1@qq.com",
-		"code":  "123456",
+		"email": email,
+		"code":  code,
 	}
 	// stringfy
 	bytes, _ := json.Marshal(reqBody)
@@ -34,4 +49,11 @@ func TestSession(t *testing.T) {
 	r.ServeHTTP(w, req)
 	// 断言
 	assert.Equal(t, w.Code, http.StatusOK)
+	// 测试 resBody 中有 jwt
+	var resBody struct {
+		JWT string `json:"jwt"`
+	}
+	if err := json.Unmarshal(w.Body.Bytes(), &resBody); err != nil {
+		t.Error("jwt is not a string")
+	}
 }
